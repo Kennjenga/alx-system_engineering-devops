d0a5ee93e06ee24b4a32d50e7362dfa9a5afa3ea
Kennjengadev <kinyagia10@gmail.com>
2024-08-24 05:11:28 -0700

    reading Reddit APis

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscribers
+"""Function that queries the Reddit API and returns the number of subscribers
 for a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
+    """Function that queries the Reddit API and returns the number of
     subscribers for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
+        try:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        except ValueError:
+            print("Error: Unable to parse JSON response")
+            return 0
     else:
+        print(f"Error: Received status code {response.status_code}")
         return 0
+
